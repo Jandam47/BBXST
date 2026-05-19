1046fb6a3d9e2ee4a88f8618453585ae23eae5af
Jandam47 <armag3don@seznam.cz>
2026-05-19 23:49:19 +0200

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,36 @@
+/* ================================================================
+   BBXST – Service Worker
+================================================================ */
+const CACHE = 'bbxst-v1';
+const OFFLINE_FILES = [
+  './BBXST.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png',
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(OFFLINE_FILES)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request)
+      .then(res => {
+        const clone = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, clone));
+        return res;
+      })
+      .catch(() => caches.match(e.request))
+  );
+});
